6a315fadc28ef0f4c6d9ab7b6edccb8262d30d61
ahmadvand <pegah.ahmadvand@gmail.com>
2019-06-16 14:38:02 +0430

    first commite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  title = 'my-maps-project';
+  title: string = 'My first AGM project';
+  lat: number = 35.6892;
+  lng: number = 51.3890;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+//https://angular-maps.com/guides/getting-started/
+import { AgmCoreModule } from '@agm/core';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      //https://churchthemes.com/page-didnt-load-google-maps-correctly/#RefererNotAllowedMapError
+      apiKey: 'AIzaSyCIRVsqTvtQmTirUQ0_8UOxqIEdcJ4wiJs'
+    })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
